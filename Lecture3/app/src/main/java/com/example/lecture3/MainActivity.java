47b97a8b60196511dba1a7383cb83a22dc62aed7
mwkangit <cabookis@gmail.com>
2021-03-02 22:17:35 +0900

    Android 3 Files

---
+++ Lecture3/app/src/main/java/com/example/lecture3/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.lecture3;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ListView listView = findViewById(R.id.listView);
+        listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+                new String[]{"copy","past","cut","delete","convert","open", "copy","past","cut","delete","convert","open", "copy","past","cut","delete","convert","open"}));
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), position+" 번째 값 : " + parent.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
+
---
+++ Lecture5/app/src/main/java/com/example/lecture5/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.lecture5;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn; // 버튼을 추가하는 버튼
+    LinearLayout View; // 추가하는 버튼을 놓을 Layout
+    int count = 0; // 버튼의 개수를 제한하는 변수
+    Drawable drawable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        DisplayMetrics dm = getResources().getDisplayMetrics(); // dp 형식의 변수를 만든다
+        int size = Math.round(20 * dm.density); // dp 의 값을 정의한다
+        
+        btn = (Button) findViewById(R.id.btn);
+
+        Button button[] = new Button[10]; // 버튼의 개수를 정의한다
+        for(int i = 0 ; i < 10 ; i++){
+            button[i] = new Button(this);
+        }
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams( // xml 에 추가 될 View의 속성을 정의하는 객체를 만든다
+                LinearLayout.LayoutParams.WRAP_CONTENT, // View 의 width 를 정의한다
+                LinearLayout.LayoutParams.WRAP_CONTENT  // View 의 height 를 정의한다
+        );
+
+        params.topMargin = size;  // 버튼 사이의 Margin 을 정의한다
+        params.gravity = Gravity.CENTER; // 버튼을 CENTER 로 정렬
+
+        View = (LinearLayout) findViewById(R.id.lay); // 추가할 버튼을 배치할 Layout 의 아이디를 가져온다
+
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(android.view.View v) {
+                if(count < 10) { // 버튼은 10개까지만 생성 가능하다
+                    button[count].setText("abc"); // 버튼의 text 를 정의한다
+                    button[count].setTextColor(Color.parseColor("#000000")); // text 의 색깔을 정의해준다
+                    button[count].setBackgroundResource(R.drawable.button_round); // drawable xml 파일을 불러와서 button 에 둥근 속성을 준다
+                    View.addView(button[count]); // Layout 에 버튼을 추가한다
+                    button[count].setLayoutParams(params); // 버튼에 위에 설정한 View 속성을 추가한다
+                    count++;
+                } else{
+                    Toast.makeText(getApplicationContext(), "수업은 10개까지만 생성 가능합니다", Toast.LENGTH_SHORT).show(); // 버튼을 11개 이상 만들려고 할 때 Toast 해준다
+                }
+            }
+        });
+
+
+    }
+}
