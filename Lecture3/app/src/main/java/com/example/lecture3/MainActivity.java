61d8bbe20a2f92fb273c0efddf9da03401388c80
mwkangit <cabookis@gmail.com>
2021-03-09 11:28:59 +0900

    4th

--- Lecture3/app/src/main/java/com/example/lecture3/MainActivity.java
+++ Lecture3/app/src/main/java/com/example/lecture3/MainActivity.java
@@ -1,85 +1,52 @@
 package com.example.lecture3;
 
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
-import android.os.Bundle;
-import android.util.DisplayMetrics;
-import android.view.Gravity;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
-    Button btn; // 버튼을 추가하는 버튼
-    LinearLayout View; // 추가하는 버튼을 놓을 Layout
-    int count = 0; // 버튼의 개수를 제한하는 변수
-                Drawable drawable;
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+    private ArrayList<Subject> arrayList;
+    private  SubjectAdapter subjectAdapter;
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
 
-            DisplayMetrics dm = getResources().getDisplayMetrics(); // dp 형식의 변수를 만든다
-            int size = Math.round(20 * dm.density); // dp 의 값을 정의한다
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-            btn = (Button) findViewById(R.id.btn);
+        recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
+        linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(linearLayoutManager); ///방금 만든 layoutmanager을 recyclerview로 넣어줘라
 
-            Button button[] = new Button[10]; // 버튼의 개수를 정의한다
-            for(int i = 0 ; i < 10 ; i++){
-                button[i] = new Button(this);
-            }
 
-            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams( // xml 에 추가 될 View의 속성을 정의하는 객체를 만든다
-                    LinearLayout.LayoutParams.WRAP_CONTENT, // View 의 width 를 정의한다
-                LinearLayout.LayoutParams.WRAP_CONTENT  // View 의 height 를 정의한다
-        );
+        arrayList = new ArrayList<>();
+        /// 아까 만든 MainAdapter을 가져와서 arrayList에 값을 넣어 준 것이다
 
-        params.topMargin = size;  // 버튼 사이의 Margin 을 정의한다
-        params.gravity = Gravity.CENTER; // 버튼을 CENTER 로 정렬
+        Subject subject = new Subject("잘생겨지는법", "강민우","폐강");
+        Subject subject2 = new Subject("무야호", "무도","알래스카");
 
-        View = (LinearLayout) findViewById(R.id.lay); // 추가할 버튼을 배치할 Layout 의 아이디를 가져온다
+        arrayList.add(subject);
+        arrayList.add(subject2);
 
 
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(android.view.View v) {
-                if(count < 10) { // 버튼은 10개까지만 생성 가능하다
-                    button[count].setText("abc"); // 버튼의 text 를 정의한다
-                    button[count].setTextColor(Color.parseColor("#000000")); // text 의 색깔을 정의해준다
-                    button[count].setBackgroundResource(R.drawable.button_round); // drawable xml 파일을 불러와서 button 에 둥근 속성을 준다
-                    View.addView(button[count]); // Layout 에 버튼을 추가한다
-                    button[count].setLayoutParams(params); // 버튼에 위에 설정한 View 속성을 추가한다
-                    count++;
-                } else{
-                    Toast.makeText(getApplicationContext(), "수업은 10개까지만 생성 가능합니다", Toast.LENGTH_SHORT).show(); // 버튼을 11개 이상 만들려고 할 때 Toast 해준다
-                }
-            }
-        });
-
-        RecyclerView recyclerView = findViewById(R.id.recyclerView);
-
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
-        recyclerView.setLayoutManager(layoutManager);
-        SubjectAdapter adapter = new SubjectAdapter();
+        subjectAdapter = new SubjectAdapter((arrayList));
+        subjectAdapter.notifyDataSetChanged();
 
 
-        adapter.addItem(new Subject("ICT개론", "윤경로", "1 : 30"));
-        adapter.addItem(new Subject("자료구조및알고리즘", "임상범", "2 : 30"));
-        adapter.addItem(new Subject("시스템프로그래밍", "임민규", "3 : 30"));
+        recyclerView.setAdapter(subjectAdapter); ///담아져있는 데이터를 recyclerView에 담아줘라
 
-        recyclerView.setAdapter(adapter);
+
+
 
 
     }
 }
-
--- Lecture3/app/src/main/java/com/example/lecture3/SubjectAdapter.java
+++ Lecture3/app/src/main/java/com/example/lecture3/SubjectAdapter.java
@@ -4,6 +4,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,68 +13,82 @@ import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
-public class SubjectAdapter extends RecyclerView.Adapter<SubjectAdapter.ViewHolder> {
-    ArrayList<Subject> items = new ArrayList<>();
+public class SubjectAdapter extends RecyclerView.Adapter<SubjectAdapter.CustomViewHolder> {
+
+    private ArrayList<Subject> arrayList;
+
+    public SubjectAdapter(ArrayList<Subject> arrayList) {
+        this.arrayList = arrayList;
+    }
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-        View itemView = inflater.inflate(R.layout.subject_item, parent, false);
+    /// listview 메뉴가 처음 생성될 때 생명주기를 뜻한다
+    public SubjectAdapter.CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.subject_item, parent, false);
+        CustomViewHolder holder = new CustomViewHolder(view);
 
-        return new ViewHolder(itemView);
+        return holder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        Subject item = items.get(position);
-        holder.setItem(item);
+    ///실제 추가 될 때에 대한 생명주기
+    public void onBindViewHolder(@NonNull SubjectAdapter.CustomViewHolder holder, int position) {
+        ///imageview를 생성한 애들을 가져온다
+        holder.subject_name.setText(arrayList.get(position).getSubject());
+        holder.professor_name.setText(arrayList.get(position).getProfessor());
+        holder.time_time.setText(arrayList.get(position).getTime());
+        ///listview가 클릭이 됬을 때와 long click이 됬을 때를 나타내 줄 수 있다
+        holder.itemView.setTag(position); /// position값을 가져온다
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String curName = holder.subject_name.getText().toString(); ///리사이클러 아이템 중 클릭한 친구의 이름을 가져온다
+                Toast.makeText(v.getContext(), curName, Toast.LENGTH_SHORT).show(); /// curName을 짧은 시간동안 띄울 것이다
+
+            }
+        });
+
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            /// 바깥에서 추가버튼을 누르면 listview가 추가 되는 것은 MainActivity 쪽에다가 구현을 할 것이다
+            /// Adapter에서는 longclick을 눌렀을 때 listview를 삭제하는 것을 만들어 볼 것이다
+
+            public boolean onLongClick(View v) {
+                remove(holder.getAdapterPosition());
+
+                return true;
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return items.size();
+        return (null != arrayList ? arrayList.size() : 0);
     }
 
-    public void addItem(Subject item){
-        items.add(item);
+    public void remove(int position){
+        try{
+            arrayList.remove(position);
+            notifyItemRemoved(position); /// notify는 새로고침이라는 뜻으로 listview를 지우고 새로고침 해주는 것이다
+        } catch(IndexOutOfBoundsException ex){
+            ex.printStackTrace();
+        }
+
     }
 
-    public void setItems(ArrayList<Subject> items){
-        this.items = items;
-    }
+    public class CustomViewHolder extends RecyclerView.ViewHolder {
 
-    public Subject getItem(int position){
-        return items.get(position);
-    }
+        protected TextView subject_name;
+        protected TextView professor_name;
+        protected TextView time_time;
 
-    public Subject setItem(int position, Subject item){
-        return items.set(position, item);
-    }
-
-    static class ViewHolder extends RecyclerView.ViewHolder {
-
-        TextView subject_name;
-        TextView professor_name;
-        TextView time_time;
-
-        public ViewHolder(@NonNull View itemView) {
+        public CustomViewHolder(@NonNull View itemView) {
             super(itemView);
-
-            subject_name = itemView.findViewById(R.id.subject_name);
-            professor_name = itemView.findViewById(R.id.professor_name);
-            time_time = itemView.findViewById(R.id.time_time);
-
-        }
-
-        public void setItem(Subject item){
-            subject_name.setText(item.getSubject());
-            professor_name.setText(item.getProfessor());
-            time_time.setText(item.getTime());
+            this.subject_name =(TextView) itemView.findViewById(R.id.subject_name); ///activity형 클래스 파일이 아니기 때문에 itemview를 가져온다
+            this.professor_name = (TextView) itemView.findViewById(R.id.professor_name);
+            this.time_time = (TextView) itemView.findViewById(R.id.time_time);
 
         }
-
-
-
     }
 }
